009611ce8673c93948689bfa4b4f7ad1ec494c7c
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-12-04 10:22:08 -0700

    tried to add more then one

--- test2.rb
+++ test2.rb
@@ -1,6 +1,8 @@
 require 'mechanize'
+		
 	
 	mechanize = Mechanize.new
+	
 	# place = mechanize.get('http://www.onthesnow.com/colorado/skireport.html')
 	# page = mechanize.get('http://www.onthesnow.com/colorado/skireport.html')
 
@@ -10,10 +12,16 @@ require 'mechanize'
 
 	#http://readysteadycode.com/howto-scrape-websites-with-ruby-and-mechanize
 	#http://jsoup.org/cookbook/extracting-data/selector-syntax
-
-
-place = mechanize.get('http://www.onthesnow.com/colorado/skireport.html')
-page = mechanize.get('http://www.onthesnow.com/colorado/skireport.html')
-puts place.at('.rowB .rLeft .name').text
-puts page.at('.rowB .rMid.c b').text
 	
+		place = mechanize.get('http://www.onthesnow.com/colorado/skireport.html')
+		page = mechanize.get('http://www.onthesnow.com/colorado/skireport.html')
+		wp_name = print place.at('.rowB .rLeft .name').text
+		print " "
+		wp_snow = print page.at('.rowB .rMid.c b').text
+		#puts "drive time: 20 min"
+
+		copper_name = print place.at('.rowD .rLeft .name').text
+		print " "
+		copper_snow = print page.at('.rowB .rMid.c b').text
+
+
