3a80e5376723d8216130a94183266a4f9a491ac7
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-12-09 10:34:54 -0700

    prints them using a method for less lines of code

--- test.rb
+++ test.rb
@@ -1,19 +1,34 @@
 require 'mechanize'
 class Test
+	def initialize
+		@agent = Mechanize.new
+		@resort_name = @agent.get('http://www.onthesnow.com/colorado/skireport.html').search("tr.rowB")
+		@snow = @agent.get('http://www.onthesnow.com/colorado/skireport.html').search("td.rMid.c")
+		
+	end
+		
 	
-agent = Mechanize.new
-resort_name = agent.get('http://www.onthesnow.com/colorado/skireport.html').search("tr.rowB")
-snow = agent.get('http://www.onthesnow.com/colorado/skireport.html').search("td.rMid.c")
+	def do_stuff
+		name_snow(@resort_name[0], @snow[2])
+		name_snow(@resort_name[4], @snow[10])
+	end
 
-#prints winter park
-print resort_name[0].search('.name').text
-print" "
-puts snow[2].search('b').text
-
-#prints vail
-print resort_name[3].search('.name').text
-print" "
-puts snow[8].search('b').text
+	def name_snow name, snow
+		#long_array = []
+		print name.search('.name').text
+		print" "
+		puts snow.search('b').text
+	end
 
 end
 
+test = Test.new
+test.do_stuff
+
+
+# prints vail
+# print resort_name[4].search('.name').text
+# print" "
+# puts vail_snow = snow[10].search('b').text	
+
+	
